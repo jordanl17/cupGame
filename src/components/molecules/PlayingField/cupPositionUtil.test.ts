3c50538d5b1c18e76eee9f3974d61db9b9bdd38d
Jordan Lawrence <jordan@firstbasehq.com>
2023-02-05 17:28:04 +0000

    testing for cup position util

---
+++ src/components/molecules/PlayingField/cupPositionUtil.test.ts
@@ -0,0 +1,35 @@
+import randomiseCupPositions from "./cupPositionUtil";
+
+const mockRandom = jest.spyOn(global.Math, "random");
+
+describe("randomiseCupPositions", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("will give cup positions for 3 cups", () => {
+    mockRandom.mockReturnValueOnce(1).mockReturnValueOnce(1);
+
+    const result = randomiseCupPositions([0, 1, 2]);
+
+    expect(result).toEqual([2, 1, 0]);
+    expect(mockRandom).toHaveBeenCalledTimes(2);
+  });
+
+  it("will always give different positions than the input", () => {
+    mockRandom
+      // make first cup go to position 0
+      .mockReturnValueOnce(0)
+      // make second cup go to position 1; third cup go to position 2
+      .mockReturnValueOnce(0)
+      // this is same as input - make second iteration move first cup to position 2
+      .mockReturnValueOnce(1)
+      // make second cup go to position 1, third cup to go position 0
+      .mockReturnValueOnce(0);
+
+    const result = randomiseCupPositions([0, 1, 2]);
+
+    expect(result).toEqual([2, 0, 1]);
+    expect(mockRandom).toHaveBeenCalledTimes(4);
+  });
+});
